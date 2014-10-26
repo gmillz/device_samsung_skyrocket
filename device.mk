e55a337864551b78f4508fbc84188f3acfa34ac3
Ethan <cannondalev2000@gmail.com>
2014-10-26 14:19:52 -0700

    F2FS
    Change-Id: I5ab346fcf8066705938c4f0317c3718c7dbcc949
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,11 @@ $(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc
