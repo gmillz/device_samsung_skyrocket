7e7074e42b99919eb439ead0cad34021e02675b0
Dan Cartier <nospamdan11@gmail.com>
2015-03-11 20:49:31 -0400

    skyrocket: Update build fingerprint to Lollipop
    Change-Id: Ifd5cbf198b61cf3ec7d8a60b96b5dc5c8d361d16

--- candy5.mk
+++ candy5.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
 # Inherit some common Candy5 stuff.
 $(call inherit-product, vendor/candy5/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/JZO54K/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 JZO54K I727UCMC1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:5.0.2/LRX22G/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 5.0.2 LRX22G I727UCMC1 release-keys"
 
 PRODUCT_NAME := candy5_skyrocket
 PRODUCT_DEVICE := skyrocket
